403a23a01e90580f8d87e71f78baf9ec2187c343
jessesteele <jessesteele@gmail.com>
2023-05-14 16:14:21 +0800

    Cleanup

--- donjon/digverbs.sh
+++ donjon/digverbs.sh
@@ -2,6 +2,9 @@
 # inkVerb donjon asset, verb.ink
 ## This is run as a fast-repeating cron task at initial VPS setup to run inkCert for hosted verb domains as soon as their DNS records have populated
 
+/usr/bin/mkdir -p /opt/verb/dig
+/usr/bin/touch /opt/verb/dig/digverbs-$(date +'%Y-%m-%d_%H:%M:%S')
+
 /opt/verb/serfs/inkdnsdigverbs
 
 e="$?"; [[ "$e" = "0" ]] || exit "$e"
