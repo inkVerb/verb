f049617ea030321b16ff227632f2feefa66fb7ca
jessesteele <jessesteele@gmail.com>
2023-05-17 17:16:35 +0800

    Cleanup

--- donjon/digdomain.sh
+++ donjon/digdomain.sh
@@ -2,8 +2,6 @@
 # inkVerb donjon asset, verb.ink
 ## This is run as a fast-repeating cron task at initial VPS setup to run inkCert for hosted verb domains as soon as their DNS records have populated
 
-/usr/bin/mkdir -p /opt/verb/dig
-
 /opt/verb/serfs/inkdnsdig "${1}" mail
 
 e="$?"; [[ "$e" = "0" ]] || exit "$e"
@@ -16,6 +14,4 @@ elif [ -n "$2" ] && [ "$2" = "wild" ]; then
   e="$?"; [[ "$e" = "0" ]] || exit "$e"
 fi
 
-/usr/bin/rm -f /opt/verb/dig/digverbs-"${1}"-*
-if [ "$(/usr/bin/ls /opt/verb/dig)" = "" ]; then /usr/bin/rm -rf /opt/verb/dig; fi
 /usr/bin/rm -f "/etc/cron.d/digdomain-${1}"
--- donjon/digverbs.sh
+++ donjon/digverbs.sh
@@ -2,14 +2,10 @@
 # inkVerb donjon asset, verb.ink
 ## This is run as a fast-repeating cron task at initial VPS setup to run inkCert for hosted verb domains as soon as their DNS records have populated
 
-/usr/bin/mkdir -p /opt/verb/dig
-
 /opt/verb/serfs/inkdnsdigverbs
 e="$?"; [[ "$e" = "0" ]] || exit "$e"
 
 /opt/verb/serfs/inkcertdole-all-verbs
 e="$?"; [[ "$e" = "0" ]] || exit "$e"
 
-/usr/bin/rm -f /opt/verb/dig/digverbs-*
-if [ "$(/usr/bin/ls /opt/verb/dig)" = "" ]; then /usr/bin/rm -rf /opt/verb/dig; fi
 /usr/bin/rm -f /etc/cron.d/digverbs
