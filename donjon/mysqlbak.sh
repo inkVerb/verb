081e5b4b66789f86c8e5638251329336c1d8477e
jessesteele <jessesteele@gmail.com>
2023-07-09 05:05:05 +0800

    MySQL backups

---
+++ donjon/mysqlbak.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# inkVerb donjon asset, verb.ink
+## This script backs up the mysql directory based with automatic preference for what drives are mounted
+
+if [ -z "$1" ]; then
+  /usr/bin/echo "Must argue the backup suffix"
+  exit 5
+fi
+
+backsuffix="$1"
+
+if [ -e "/mnt/hdd" ]; then
+  backdir="/mnt/hdd/mysql.bak.d"
+elif [ -e "/mnt/ssd" ]; then
+  backdir="/mnt/ssd/mysql.bak.d"
+else
+  backdir="/var/lib/mysql.bak.d"
+el
+
+/usr/bin/mkdir -p ${backdir}
+  
+if /usr/bin/systemctl is-active mysql; then
+  # Service check
+  /usr/bin/systemctl stop mysql
+  
+  # Remove any old backup
+  if [ -d "${backdir}/mysql.${backsuffix}.old" ]; then
+    /usr/bin/rm -rf "${backdir}/mysql.${backsuffix}.old"
+  fi
+  
+  # Displace any backup
+  if [ -d "${backdir}/mysql.${backsuffix}" ]; then
+    /usr/bin/mv "${backdir}/mysql.${backsuffix}" "${backdir}/mysql.${backsuffix}.old"
+  fi
+
+  # Copy the directory
+  /usr/bin/cp -r /var/lib/mysql "${backdir}/mysql.${backsuffix}"
+  /usr/bin/systemctl start mysql
+fi
+
